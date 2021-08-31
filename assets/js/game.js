35faf49d913ea4e1ca3063ec1498a5521da71b5f
Nadine <nadine_paez@hotmail.com>
2021-08-30 21:39:30 -0400

    initial setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+//this is how to write comments
+//declare function
+function fight() {
+    window.alert("The fight has begun")
+}
+//prompt and user input assignment
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message")
+//this will do math
+console.log(10+10);
+console.log("Our robot's name is " + playerName)
+//call function
+//fight();
